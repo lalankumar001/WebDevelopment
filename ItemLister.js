4d31ee87568823bd37bf93897f51792e27f09d6e
lalankumar001 <tocsi.org@gmail.com>
2023-01-02 23:33:59 +0530

    querySelector

--- ItemLister.js
+++ ItemLister.js
@@ -1,7 +1,11 @@
-console.dir(document);
-console.log(document.doctype);
-console.log(document.lastElementChild);
+// console.dir(document);
+// console.log(document.doctype);
+// console.log(document.lastElementChild);
 
 // GetElementById
 
-console.log(document.getElementById('header-title'));
+// console.log(document.getElementById('header-title'));
+
+var selectThirdItem = document.querySelector('list-group-item:nth-child(2)');
+    selectThirdItem.style.color='green';
+    selectThirdItem.style.font='bold';
